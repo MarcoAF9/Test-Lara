13170a4a5068d8854a38a02cbf08a43a52e6a9ec
Mark4356 <159771320+MarcoAF9@users.noreply.github.com>
2026-06-08 23:13:44 +0300

    tessth

--- resources/views/Job/index.blade.php
+++ resources/views/Job/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Jobs Board</title>
 </head>
 <body>
     <h1>Job Board</h1>
